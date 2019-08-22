cae46a05c8f8246dc2c9bbb6fad7229565dcc321
zhuanglunzhi <zhuanglunzhi@followme.cn>
2019-08-22 11:28:53 +0800

    fix : 删除掉clear-dockers命令

--- zsh/.zsh/my.zsh
+++ zsh/.zsh/my.zsh
@@ -4,7 +4,6 @@ PROJECT="$HOME/Project"
 # 自定义命令
 alias webstorm="$APPDIR/WebStorm-183.5153.33/bin/webstorm.sh &"
 alias datagrip="$APPDIR/DataGrip-2018.3.1/bin/datagrip.sh &"
-alias clear-dockers="node $PROJECT/myfm/zlz/shell/clear-dockers.js"
 alias shadowsocks-app="$APPDIR/Shadowsocks-app/Shadowsocks-Qt5-3.0.1-x86_64.AppImage &"
 
 alias cat='ccat'
